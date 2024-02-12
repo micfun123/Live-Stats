7271b95021296f6149692c5ee112b1301ad15554
micfun123 <54761507+micfun123@users.noreply.github.com>
2024-02-12 21:23:53 +0000

    foodWastedPerSecond

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const devorceCounterElement = document.getElementById('divorceCounter');
 const videosuploadelement = document.getElementById('videosUploaded');
 const co2CounterElement = document.getElementById('co2Counter');
 const clothingPerSecondElement = document.getElementById('clothingPerSecond');
+const foodwastedPerSecondElement = document.getElementById('foodWastedPerSecond');
 const birthsPerSecond = 4.5;
 const deathsPerSecond = 1.8;
 const hoursofvideouploadedpersecond = 8.3333333333333;
@@ -14,6 +15,7 @@ const marriagesPerSecond = 1.74;
 const devorcePerSecond = 1.4;
 const co2PerSecond = 1337;
 const clothingPerSecond = 2.916;
+const foodwastedPerSecond = 66;
 
 
 function updateTime() {
@@ -27,6 +29,7 @@ function updateTime() {
     const numberOfMarriages = Math.floor(seconds * marriagesPerSecond);
     const numberOfDevorces = Math.floor(seconds * devorcePerSecond);
     const co2 = Math.floor(seconds * co2PerSecond);
+    const foodwaste = Math.floor(seconds * foodwastedPerSecond);
 
 
     // Batch DOM updates
@@ -38,6 +41,8 @@ function updateTime() {
     devorceCounterElement.textContent = "Number of devorces during your visit: " + numberOfDevorces;
     co2CounterElement.textContent = "Amount of CO2 emitted during your visit: " + co2 + " tonnes";
     clothingPerSecondElement.textContent = "The amount of clothing thrown out during your visit: " + Math.floor(seconds * clothingPerSecond) + " Tonnes";
+    foodwastedPerSecondElement.textContent = "The amount of food wasted during your visit: " + foodwaste + " Tonnes";
+    
 }
 
 setInterval(updateTime, 100); // Update every 100 milliseconds
